dccfd80803a32543d23170f748ee185914c6d05c
triusdeil <triusdeil@gmail.com>
2020-11-25 19:19:37 -0400

    enviar imagenes, config el emtodo post de envio, index.ejs, config lla aplicacion

--- src/index.js
+++ src/index.js
@@ -1,12 +1,47 @@
 const express = require('express')
-
-//initilizacions 
 const app = express()
+const ejs = require('ejs')
+const path = require('path')
+const multer = require('multer')
 
-//settings
+//establecer como guardar las imagenes
+const storage = multer.diskStorage({
+    destination: path.join(__dirname,('public/uploads')),
+    filename: (req, file, cb) =>{
+        cb(null, file.originalname)
+    }
+})
+//**settings
 app.set('port', 4000)
 
-//server 
-app.listen(app.get('port'), () => {
-    console.log(`server listening ${app.get('port')}`)
+//establecer la direccion de la carpeta
+//con esto informamos que la carpeta views esta en src
+app.set('views', path.join(__dirname, 'views'))
+//configurar el motor de las vistas
+
+//**ejs
+app.set('view engine', 'ejs')
+
+//middleware
+app.use(multer({
+    storage: storage,
+    dest: path.join(__dirname, 'public/uploads')
+}).single('image'))
+
+//**routes
+//recibir lo que el formulario esta enviando
+app.post('/upload', (req,res) =>{
+    console.log(req.file)
+    res.send('uploaded')
+})
+//manejador de peticiones
+//res.render enviar pagina ejs html
+app.get('/', (req,res)=>{
+    res.render('index')
+})
+
+//**server
+
+app.listen(app.get('port'),() =>{
+    console.log(`server listen on port: ${app.get('port')}`)
 })
